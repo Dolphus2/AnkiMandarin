4c52f88e920999aa51a77588db01679dd2bf2cbb
gabriel-desktop-omarchy <gabrielruge@gmail.com>
2026-04-26 20:00:52 +0200

    mkv tools update

--- anki_mandarin/mkv_subs/__init__.py
+++
@@ -1,3 +0,0 @@
-from anki_mandarin.mkv_subs.mkv_sub_utils import load_mkv_track_info, select_subtitle_track, extract_subtitles, \
-    add_simplified_sub_to_mkv, batch_add_simplified_subs
-from anki_mandarin.mkv_subs.convert_subtitles import convert_subtitles_to_simplified
